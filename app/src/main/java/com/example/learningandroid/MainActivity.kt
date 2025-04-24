d2f45121bfa3720b67176c07d105cebfc53a4b31
Avishek Gyanchand <agyancha@agyancha-mn3.linkedin.biz>
2025-04-23 23:19:23 -0700

    Testing Preview and Emulator greeting update

--- app/src/main/java/com/example/learningandroid/MainActivity.kt
+++ app/src/main/java/com/example/learningandroid/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             LearningAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Soupy",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LearningAndroidTheme {
-        Greeting("Android")
+        Greeting("Soupy!!")
     }
 }
